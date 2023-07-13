1a7203fd9a6aa0dcdcc591ae1ff1507487aa6a73
jiinse0 <jee_ji@naver.com>
2023-07-13 23:24:45 +0900

    [feat] commentList 수정, import 정리

--- src/main/java/com/sparta/spring_lv3/dto/PostResponseDto.java
+++ src/main/java/com/sparta/spring_lv3/dto/PostResponseDto.java
@@ -1,6 +1,5 @@
 package com.sparta.spring_lv3.dto;
 
-import com.sparta.spring_lv3.entity.Comment;
 import com.sparta.spring_lv3.entity.Post;
 import lombok.Getter;
 
@@ -27,13 +26,14 @@ public class PostResponseDto {
         this.createdAt = post.getCreateAt();
         this.modifiedAt = post.getModifiedAt();
 
-        this.commentList = post.getCommentList().stream()
+        this.commentList.addAll(post.getCommentList()
+                .stream()
                 .map(CommentResponseDto::new)
                 // .sorted() : 스트림 요소를 정렬
                 // Comparator.comparing() : 정렬 기준 지정
                 //                          CommentResponseDto 객체의 createdAt 속성을 기준으로 정렬
                 // .reversed() : 정렬 순서를 반대로 변경 (내림차순)
                 .sorted(Comparator.comparing(CommentResponseDto::getCreatedAt).reversed())
-                .toList();
+                .toList());
     }
 }
