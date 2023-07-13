685e14ca73df89bcf2833b73359eedaec2417137
jiinse0 <jee_ji@naver.com>
2023-07-13 23:23:25 +0900

    [feat] commentList, username 추가

--- src/main/java/com/sparta/spring_lv3/entity/Post.java
+++ src/main/java/com/sparta/spring_lv3/entity/Post.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -16,6 +17,9 @@ public class Post extends Timestamped{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "post_id")
     private Long id;
+
+    @Column
+    private String username;
 
     @Column(nullable = false)
     private String title;
@@ -30,14 +34,13 @@ public class Post extends Timestamped{
     // @OneToMany : 일대다
     // mappedBy : 연관관계의 주인을 지정
     //            "post"로 설정되어 있기 때문에 Post 엔티티가 연관관계의 주인
-    // cascade : 연관된 엔티티에 대한 영속성 작업을 어떻게 처리할지를 설정
-    // CascadeType.REMOVE : Post 엔티티가 삭제될 때 연관된 모든 Comment 엔티티도 함께 삭제되도록 설정
-    @OneToMany(mappedBy = "post", cascade = CascadeType.REMOVE)
-    private List<Comment> commentList;
+    @OneToMany(mappedBy = "post")
+    private List<Comment> commentList = new ArrayList<>();
 
     public Post(PostRequestDto requestDto, User user) {
         this.title = requestDto.getTitle();
         this.content = requestDto.getContent();
+        this.username = user.getUsername();
         this.user = user;
     }
 
